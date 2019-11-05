afc7b1c6f7cef2a1e741a0b665c0dfb34d5c3d35
iamraghudb <54179651+iamraghudb@users.noreply.github.com>
2019-11-05 10:05:07 +0530

    Updates

---
+++ precache-manifest.594472f58cad377db7b9625e07f096fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040402b83560100dc464abe2f1b0b3bf",
+    "url": "/iamraghudb/Robo-Friends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/iamraghudb/Robo-Friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "eb3eb0e641bbda16e702",
+    "url": "/iamraghudb/Robo-Friends/static/css/main.47471f58.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/iamraghudb/Robo-Friends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "eb3eb0e641bbda16e702",
+    "url": "/iamraghudb/Robo-Friends/static/js/main.f80d93a2.chunk.js"
+  },
+  {
+    "revision": "d9dfebfd072fef08e1b5",
+    "url": "/iamraghudb/Robo-Friends/static/js/runtime-main.bfbd9718.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/iamraghudb/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
